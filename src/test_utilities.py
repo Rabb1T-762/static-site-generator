370ef887011476400aba82ee86448e0c85299d28
Rabb1T <grahamtaylor1811@gmail.com>
2024-07-14 15:22:01 +0200

    Added tests for text_node_to_html_node utility

--- src/test_utilities.py
+++ src/test_utilities.py
@@ -6,7 +6,7 @@ from leafnode import LeafNode
 
 
 class Test_TextNodeToHtmlNode(unittest.TestCase):
-    def test_text_node_to_html_node_unknown_type(self):
+    def test_is_passed_unknown_type(self):
         # Arrange
         # Act
         text_node = TextNode("Unknown Text", "unknown")
@@ -15,7 +15,7 @@ class Test_TextNodeToHtmlNode(unittest.TestCase):
         with self.assertRaises(ValueError):
             text_node_to_html_node(text_node)
 
-    def test_text_node_to_html_node_is_known_type_returns_leafNode(self):
+    def test_is_passed_text_type_and_returns_leafNode(self):
         # Arrange
         text_node = TextNode("Known Text Type", "text")
         expected = LeafNode
@@ -25,3 +25,68 @@ class Test_TextNodeToHtmlNode(unittest.TestCase):
 
         # Assert
         self.assertIsInstance(result, expected)
+
+    def test_is_passed_text_type_and_returns_leafNode_with_no_tag_and_text(self):
+        # Arrange
+        text_node = TextNode("Expected Text", "text")
+        expected = "Expected Text"
+        # Act
+        actual = text_node_to_html_node(text_node)
+
+        # Assert
+        self.assertEqual(actual.to_html(), expected)
+
+    def test_is_passed_bold_type_and_returns_leafNode_with_b_tag_and_text(self):
+        # Arrange
+        text_node = TextNode("Expected Text", "bold")
+        expected = "<b>Expected Text</b>"
+
+        # Act
+        actual = text_node_to_html_node(text_node)
+
+        # Assert
+        self.assertEqual(actual.to_html(), expected)
+
+    def test_is_passed_italic_type_and_returns_leafNode_with_i_tag_and_text(self):
+        # Arrange
+        text_node = TextNode("Expected Text", "italic")
+        expected = "<i>Expected Text</i>"
+
+        # Act
+        actual = text_node_to_html_node(text_node)
+
+        # Assert
+        self.assertEqual(actual.to_html(), expected)
+
+    def test_is_passed_code_type_and_returns_leafNode_with_code_tag_and_text(self):
+        # Arrange
+        text_node = TextNode("print 'Hello World!'", "code")
+        expected = "<code>print 'Hello World!'</code>"
+        # Act
+        actual = text_node_to_html_node(text_node)
+
+        # Assert
+        self.assertEqual(actual.to_html(), expected)
+
+    def test_is_passed_link_type_and_returns_leafNode_with_a_tag_and_anchor_text_and_href_prop(self):
+        # Arrange
+        text_node = TextNode("Click Me!", "link", "https://www.google.com")
+        expected = '<a href="https://www.google.com">Click Me!</a>'
+
+        # Act
+        actual = text_node_to_html_node(text_node)
+
+        # Assert
+        self.assertEqual(actual.to_html(), expected)
+
+    def test_is_passed_image_type_and_returns_leaf_node_with_img_tag_and_src_alt_props(self):
+        # Arrange
+        text_node = TextNode("Image Alt Text", "image",
+                             "http://example.com/image.png")
+        expected_html = '<img src="http://example.com/image.png" alt="Image Alt Text"></img>'
+    
+        # Act
+        actual = text_node_to_html_node(text_node)
+    
+        # Assert
+        self.assertEqual(actual.to_html(), expected_html)
--- src/utilities.py
+++ src/utilities.py
@@ -1,10 +1,15 @@
 from leafnode import LeafNode
-from textnode import TextNode
 
 
 def text_node_to_html_node(text_node):
-    types = ["text"]
+    types = {"text": LeafNode("", text_node.text),
+             "bold": LeafNode("b", text_node.text),
+             "italic": LeafNode("i", text_node.text),
+             "code": LeafNode("code", text_node.text),
+             "link": LeafNode("a", text_node.text, {"href": text_node.url}),
+             "image": LeafNode("img", "", {"src": text_node.url, "alt": text_node.text}),
+             }
     if text_node.text_type not in types:
         raise ValueError(f"Unknown TextNode Type: {text_node}")
 
-    return LeafNode("", text_node.text)
+    return types[text_node.text_type]
